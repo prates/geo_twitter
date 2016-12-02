0bd8daba33dc4b64cbbd2e32b7a2700dcf44a242
Alexandre Prates <alexandre.b.prates@gmail.com>
2016-12-02 21:00:49 -0200

    add files

---
+++ app/CONFIG.py
@@ -0,0 +1,9 @@
+
+#Tempo de retencao dos mapas
+TEMPORENTENCAO = 60 #Valor em segundos
+
+#Localizacao do centor do map
+COORDENADASMAPA = [-23.5505, -46.6333]
+
+REDIS = {'host': 'localhost', 'port':6379, 'db':0}
+
